277db03e2f605acecb8fbc6e185a5b1b0099f1da
Daniel Smith <jellymann@gmail.com>
2018-03-10 01:04:03 +0200

    Initial Commit

---
+++ NfsmwConfig/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NfsmwConfig
+//
+//  Created by Daniel Smith on 2018/03/10.
+//  Copyright © 2018 sourlemon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NfsmwConfig/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NfsmwConfig
+//
+//  Created by Daniel Smith on 2018/03/10.
+//  Copyright © 2018 sourlemon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
